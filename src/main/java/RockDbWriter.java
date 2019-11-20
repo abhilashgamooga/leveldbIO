886cd2f6f007d26080c32040729e32c73a7fdd65
abhilash aruva <abhilash.aruva@gamooga.com>
2019-11-20 12:28:23 +0530

    debug

--- src/main/java/RockDbWriter.java
+++ src/main/java/RockDbWriter.java
@@ -18,48 +18,49 @@ public class RockDbWriter {
     private RocksDB writeDb;
     public RockDbWriter() {
         // a static method that loads the RocksDB C++ library.
-        RocksDB.loadLibrary();
-        File dbPath = Paths.get("/data/master/0/2019-6/").toFile();
-        File dbWritePath = Paths.get("/data/leveldb/master/0/2019-6/").toFile();
-        org.iq80.leveldb.Options options = new org.iq80.leveldb.Options();
-        options.createIfMissing(true);
-        options.writeBufferSize(256 << 20);
-        options.blockSize(64 * 1024);
-        options.compressionType(CompressionType.SNAPPY);
-        try {
-            this.readDb = factory.open(dbPath, options);
-            this.writeLevelDb = factory.open(dbWritePath, options);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // the Options class contains a set of configurable DB options
-        // that determines the behaviour of the database.
-       try {
-           this.writeDb = RocksDB.open( "/data/rocksdb/master/0/2019-6");
-        } catch (RocksDBException e) {
-            // do some error handling
-            e.printStackTrace();
-        }
+
     }
     public void start() throws RocksDBException, IOException {
-        WriteOptions options = new WriteOptions();
-        options.disableWAL();
+        for(int i=1; i < 8; i++) {
+            for(int y = 8; y < 10; y++) {
+                RocksDB.loadLibrary();
+                File dbPath = Paths.get("/data/master/" + i + "/2019-"+y+"/").toFile();
+                org.iq80.leveldb.Options options = new org.iq80.leveldb.Options();
+                options.createIfMissing(true);
+                options.writeBufferSize(256 << 20);
+                options.blockSize(64 * 1024);
+                options.compressionType(CompressionType.SNAPPY);
+                try {
+                    this.readDb = factory.open(dbPath, options);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // the Options class contains a set of configurable DB options
+                // that determines the behaviour of the database.
+                try {
+                    this.writeDb = RocksDB.open("/data/rocksdb/master/" + i + "/2019-"+y+"/");
+                } catch (RocksDBException e) {
+                    // do some error handling
+                    e.printStackTrace();
+                }
 
-        DBIterator iterator = this.readDb.iterator();
-        int i =0;
-        System.out.println("Leveldb Read Start - "+System.currentTimeMillis());
-        for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
-            if (i > 10000000) {
-                i = 0;
-                System.out.println("read 10000000");
+                DBIterator iterator = this.readDb.iterator();
+                int c = 0;
+                System.out.println("Reading "+i+" month "+y+" Start - " + System.currentTimeMillis());
+                for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
+                    if (c > 10000000) {
+                        c = 0;
+                        System.out.println("read 10000000");
+                    }
+                    byte[] key = (iterator.peekNext().getKey());
+                    byte[] val = (iterator.peekNext().getKey());
+                    this.writeDb.put(key, val);
+                    this.writeLevelDb.put(key, val);
+                    ++c;
+                }
+                System.out.println("write completed "+i+" month "+y+" ----------------");
             }
-            byte[] key = (iterator.peekNext().getKey());
-            byte[] val = (iterator.peekNext().getKey());
-            this.writeDb.put(key, val);
-            this.writeLevelDb.put(key, val);
-            ++i;
         }
-        System.out.println("write completed");
 
 
     }
